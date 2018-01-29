5601014bfe98e79026d8f30afdf8c13091d61bf6
Pavel Lukyanov <panya225@gmail.com>
2018-01-29 15:16:37 +0300

    2nd working version with unic vars in classes

--- classes_for_db.py
+++ classes_for_db.py
@@ -3,7 +3,7 @@ from abc import ABCMeta
 
 
 class Events(object):
-    def __init__(self, created=None, code=None, severity=None, category=None, sourceType=None, sourceId=None):
+    def __init__(self, code, created=None, severity=None, category=None, sourceType=None, sourceId=None):
         self.created = created  # date of creation
         self.code = code  # code
         self.severity = severity  # severity
@@ -32,7 +32,7 @@ class Events(object):
 
 
 class ZCandUS(Events):
-    def __init__(self, zcName=None, ccName=None, operaton=None, services=None, operStatus=None, parent=None):
+    def __init__(self, code, zcName=None, ccName=None, operaton=None, services=None, operStatus=None, parent=None):
         super(ZCandUS, self).__init__(parent)  # init from super class with same params
         # optional params
         self.services = services
@@ -40,10 +40,10 @@ class ZCandUS(Events):
         self.zcName = zcName
         self.operStatus = operStatus
         self.ccName = ccName
-
+        self.code = code
 
 class TKO(ZCandUS):
-    def __init__(self, segment=None, unitName=None, unitType=None,
+    def __init__(self, code, segment=None, unitName=None, unitType=None,
                  message=None, errorMsg=None, parent=None):
         super(TKO, self).__init__(parent)
         self.segment = segment
@@ -51,10 +51,10 @@ class TKO(ZCandUS):
         self.unitType = unitType
         self.message = message
         self.errorMsg = errorMsg
-
+        self.code = code
 
 class OM(ZCandUS):
-    def __init__(self, objName=None, className=None,
+    def __init__(self, code, objName=None, className=None,
                  dsName=None, dsType=None,
                  admStatus=None, errorMsg=None, parent=None):
         super(OM, self).__init__(parent)
@@ -64,10 +64,10 @@ class OM(ZCandUS):
         self.dsType = dsType
         self.admStatus = admStatus
         self.errorMsg = errorMsg
-
+        self.code = code
 
 class PS(ZCandUS):
-    def __init__(self, zcName=None, softName=None, familyName=None,
+    def __init__(self, code, zcName=None, softName=None, familyName=None,
                  operStatus=None,
                  admStatus=None, errorMsg=None, parent=None):
         super(PS, self).__init__(zcName, operStatus, parent)
@@ -75,16 +75,16 @@ class PS(ZCandUS):
         self.familyName = familyName
         self.admStatus = admStatus
         self.errorMsg = errorMsg
-
+        self.code = code
 
 class NetworkConnections(Events):
-    def __init__(self, netName=None, parent=None):
+    def __init__(self, code, netName=None, parent=None):
         super(NetworkConnections, self).__init__(parent)
         self.netName = netName
-
+        self.code = code
 
 class LinesAndTract(NetworkConnections):
-    def __init__(self, lName=None, lType=None, lpFrom=None,
+    def __init__(self, code, lName=None, lType=None, lpFrom=None,
                  lpTo=None,
                  ccName=None, segment=None, operStatus=None, errorMsg=None, parent=None):
         super(LinesAndTract, self).__init__(parent)
@@ -96,32 +96,32 @@ class LinesAndTract(NetworkConnections):
         self.segment = segment
         self.operStatus = operStatus
         self.errorMsg = errorMsg
-
+        self.code = code
 
 class UsersAndRoles(Events):
-    def __init__(self, postName=None, personName=None,
+    def __init__(self, code, postName=None, personName=None,
                  personStatus=None, message=None, parent=None):
         super(UsersAndRoles, self).__init__(parent)
         self.postName = postName
         self.personName = personName
         self.personStatus = personStatus
         self.message = message
-
+        self.code = code
 
 class JournalOfEvents(Events):
-    def __init__(self, parent=None):
+    def __init__(self, code, parent=None):
         super(JournalOfEvents, self).__init__(parent)
-
+        self.code = code
 
 class JournalOfActions(Events):
-    def __init__(self, workGUI=None, personName=None, parent=None):
+    def __init__(self, code, workGUI=None, personName=None, parent=None):
         super(JournalOfActions, self).__init__(parent)
         self.workGUI = workGUI
         self.personName = personName
-
+        self.code = code
 
 class Documents(Events):
-    def __init__(self, docName=None, docType=None, contentName=None,
+    def __init__(self, code, docName=None, docType=None, contentName=None,
                  actionType=None, email=None, parent=None):
         super(Documents, self).__init__(parent)
         self.docName = docName
@@ -129,19 +129,17 @@ class Documents(Events):
         self.contentName = contentName
         self.actionType = actionType
         self.email = email
-
+        self.code = code
 
 class MetaData(Events):
-    def __init__(self, metaName=None, taskName=None, message=None,
-                 errorMsg=None, *args, **kw):
-        super(MetaData, self).__init__(*args, **kw)
+    def __init__(self, code, metaName=None, taskName=None, message=None,
+                 errorMsg=None, parent=None):
+        super(MetaData, self).__init__(parent)
         self.metaName = metaName
         self.taskName = taskName
         self.message = message
         self.errorMsg = errorMsg
-
-
-
+        self.code = code
 
 if __name__ == '__main__':
     print type(ZCandUS())
--- vars_for_classes.py
+++ vars_for_classes.py
@@ -21,13 +21,13 @@ codeForBd = (
 paramsForCodes = {codeForBd[0]: (u'operStatus', u'zcName', u'ccName'),
                   codeForBd[1]: (u'segment', u'services', u'operation', u'zcName'),
                   codeForBd[2]: (
-                  u'operStatus', u'errorMsg', u'segment', u'unitName', u'unitType', u'zcName', u'ccName'),
+                      u'operStatus', u'errorMsg', u'segment', u'unitName', u'unitType', u'zcName', u'ccName'),
                   codeForBd[3]: (u'dsName', u'dsType', u'operStatus', u'errorMsg', u'zcName', u'ccName'),
                   codeForBd[4]: (u'objName', u'className', u'admStatus', u'zcName', u'ccName'),
                   codeForBd[5]: (u'admStatus', u'softName', u'familyName', u'zcName', u'ccName'),
                   codeForBd[6]: (u'operStatus', u'errorMsg', u'softName', u'familyName', u'zcName', u'ccName'),
                   codeForBd[7]: (u'ccName', u'segment', u'operStatus', u'errorMsg', u'lName', u'lType', u'netName'),
-                  codeForBd[8]: (u'ccName', u'segment', u'operStatus', u'errorMsg',  u'lName', u'lType', u'netName'),
+                  codeForBd[8]: (u'ccName', u'segment', u'operStatus', u'errorMsg', u'lName', u'lType', u'netName'),
                   codeForBd[9]: (u'personName', u'message', u'postName'),
                   codeForBd[10]: (u'personName', u'personStatus', u'postName'),
                   codeForBd[11]: (u'workGUI', u'personName'),
@@ -79,24 +79,24 @@ docTypeGen = lambda: random.choice(range(0, 8))
 actionTypeGen = lambda: random.choice(range(0, 13))
 
 # шаблоны событий
-addseg_oper_status_changed = ZCandUS()
-cc_service = ZCandUS()
-tko_changed = TKO()
-tko_oper_status_changed = TKO()
-ds_oper_status_changed = OM()
-obj_adm_status_changed = OM()
-soft_adm_status_changed = PS()
-soft_oper_status_changed = PS()
-line_oper_status_changed = LinesAndTract()
-tr_oper_status_changed = LinesAndTract()
-person_changed = UsersAndRoles()
-person_status_changed = UsersAndRoles()
-work_complite = JournalOfActions()
-work_comment = JournalOfActions()
-document_content_added = Documents()
-document_action_overdue = Documents()
-task_error = MetaData()
-incedent_added = MetaData()
+addseg_oper_status_changed = ZCandUS(code=u'1.1.1.10')
+cc_service = ZCandUS(code=u'1.1.1.9')
+tko_changed = TKO(code=u'1.1.1.5.5')
+tko_oper_status_changed = TKO(code=u'1.1.1.5.3')
+ds_oper_status_changed = OM(code=u'1.1.1.6.12')
+obj_adm_status_changed = OM(code=u'1.1.1.6.11')
+soft_adm_status_changed = PS(code=u'1.1.1.7.1.8')
+soft_oper_status_changed = PS(code=u'1.1.1.7.1.7')
+line_oper_status_changed = LinesAndTract(code=u'1.1.2.6.5')
+tr_oper_status_changed = LinesAndTract(code=u'1.1.2.6.6')
+person_changed = UsersAndRoles(code=u'1.2.5')
+person_status_changed = UsersAndRoles(code=u'1.2.6')
+work_complite = JournalOfActions(code=u'1.3.2.4')
+work_comment = JournalOfActions(code=u'1.3.2.6')
+document_content_added = Documents(code=u'1.3.3.3')
+document_action_overdue = Documents(code=u'1.3.3.7')
+task_error = MetaData(code=u'1.4.6')
+incedent_added = MetaData(code=u'1.1.1.5.3')
 
 # лист классов событий сверху
 listOfClasses = [addseg_oper_status_changed, cc_service, tko_changed, tko_oper_status_changed, ds_oper_status_changed,
@@ -109,11 +109,16 @@ listOfClasses = [addseg_oper_status_changed, cc_service, tko_changed, tko_oper_s
 
 def generationEvents():
     """генерация классов для описания события и просвоение значения полям этого класса"""
-    codeForMessage = codeGen().next()  # для описания кода
+    # codeForMessage = codeGen().next()  # для описания кода
     item = random.choice(listOfClasses)  # выбор класса из списка
-    setOfParams = {u'created', u'code', u'severity', u'category', u'sourceType', u'sourceId'}
+    varOfClass = {x for x in dir(item) if
+                  not x.startswith('_') and x != 'messageFromClass'}  # переменные конретного класса в множестве
+    codeForMessage = getattr(item, u'code')  # получение кода из класса
+    setOfParams = {u'created', u'code', u'severity', u'category', u'sourceType',
+                   u'sourceId'}  # множество переменных, которые должны быть 100%
     for i in paramsForCodes[codeForMessage]:
-        setOfParams.add(i)
+        setOfParams.add(i)  # добавление к множеству обязательных перменных - обязательных переменных события
+    compliteSet = setOfParams.intersection(varOfClass)  # пересечение множеств, что бы узнать какие параметры заполнять
     eventDesc = {u'created': createdGen(), u'code': codeForMessage, u'severity': severityGen(),
                  u'category': category_gen(),
                  u'sourceType': sourceTypeGen(),
@@ -130,9 +135,6 @@ def generationEvents():
                  u'docType': docTypeGen(),
                  u'contentName': stringGen(), u'actionType': actionTypeGen(), u'metaName': stringGen(),
                  u'taskName': stringGen()}  # генерируется словарь значений для полей класса
-    varOfClass = {x for x in dir(item) if
-                  not x.startswith('_') and x != 'messageFromClass'}  # получение переменных класса для заполнения
-    compliteSet = setOfParams.intersection(varOfClass)
     for key in eventDesc.keys():  # пробежка по ключам словаря
         if key in compliteSet:  # сравнение ключа с переменной, содержащейся в классе
             item.__setattr__(key, eventDesc[key])  # присвоение значения
