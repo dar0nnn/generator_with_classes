011b9d28b4ef3d9ecf8654762493ae182d5c2bd3
Pavel Lukyanov <panya225@gmail.com>
2018-02-16 12:18:09 +0300

    working. idk about astra

--- test postgres/sql_test.py
+++ test postgres/sql_test.py
@@ -1,8 +1,6 @@
 # -*- coding: UTF-8 -*-
 import csv
-import pghstore
 import datetime
-import json
 import psycopg2.extras
 import psycopg2
 import sys, locale, re
@@ -10,19 +8,22 @@ import sys, locale, re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 locale.setlocale(locale.LC_ALL, '')
-
 vars = [u'created', u'code', u'category', u'sourceType', u'sourceId', u'severity', u'description']
+# atttrs = [u'code', u'created',u'sourceType', u'sourceId' , u'category', u'severity', u'description', u'params']
 
-conn = psycopg2.connect(
-    host='192.168.62.129',
-    user='postgres',
-    port=5432,
-    password='qwe123',
-    dbname='testFromIdea')
-cursor = conn.cursor()
-psycopg2.extras.register_hstore(conn)
 try:
-    # statement = '''CREATE TABLE sql_test_dicts (
+    conn = psycopg2.connect(
+        host='192.168.62.129',
+        user='postgres',
+        port=5432,
+        password='qwe123',
+        dbname='testFromIdea',
+        client_encoding='utf-8')
+    psycopg2.extras.register_hstore(conn, globally=False, unicode=False)
+    conn.autocommit = True
+    # cursor = conn.cursor()
+    cursor = psycopg2.extras.RealDictCursor(conn)
+    # statement = '''CREATE TABLE "sql_dicts" (
     #                event_id SERIAL,
     #                code varchar(100),
     #                created date,
@@ -31,11 +32,11 @@ try:
     #                category varchar(100),
     #                severity varchar(100),
     #                description varchar(100),
-    #                value hstore
+    #                params hstore
     #                );'''
-    # for header in vars():
-    #     statement = statement + '\n{} varchar(100),'.format(header)
-    # statement = statement[:-1] + ');',
+    # # for header in vars():
+    # #     statement = statement + '\n{} varchar(100),'.format(header)
+    # # statement = statement[:-1] + ');',
     # cursor.execute(statement)
     # conn.commit()
 
@@ -44,8 +45,6 @@ try:
         dictOfParams = {}
         reader = csv.DictReader(fp)
         for row in reader:
-            listOfKeys = []
-            listOfValues = []
             for k, v in row.items():
                 if k not in vars:
                     if v != '':
@@ -57,34 +56,10 @@ try:
                     date = float(row[u'created'])
                     row[u'created'] = datetime.datetime.fromtimestamp(date)
             row[u'params'] = dictOfParams
-            for k, v in row.items():
-                listOfKeys.append(k)
-                listOfValues.append(v)
-            # print listOfKeys
-            # print listOfValues
-
-            statement = '''insert into "sql_test_dicts" ("code") values ('') returning "event_id"'''
-            cursor.execute(statement)
-            conn.commit()
+            statement = '''insert into "sql_dicts" ("code", "created","sourcetype", "sourceid" , "category", "severity", "description", "params") values (%s,%s,%s,%s,%s,%s,%s,%s) returning "event_id"'''
+            cursor.execute(statement, (row[u"code"], row[u"created"], row[u"sourceType"], row[u"sourceId"], row[u"category"], row[u"severity"],
+                row[u"description"], row[u"params"]))
             id_ = cursor.fetchall()
-            print id_[0][0]
-
-            for i in xrange(len(listOfKeys)):
-                if type(listOfValues[i]) is dict:
-                    forHstore = pghstore.dumps(listOfValues[i])
-                    # listForHstore = []
-                    # listForHstoreValues= []
-                    # for k,v in listOfValues[i].items():
-                    #     listForHstore.append(k)
-                    #     listForHstore.append(v)
-                    statement = '''update "sql_test_dicts" set ({}) = ('{}') where "event_id" = {} '''.format(listOfKeys[i], forHstore, id_[0][0])
-                else:
-                    statement = '''update "sql_test_dicts" set ({}) = ('{}') where "event_id" = {} '''.format(listOfKeys[i], listOfValues[i], id_[0][0])
-                cursor.execute(statement)
-                conn.commit()
-
-            # statement = '''insert into "sql_test_dicts" {} values '{}' '''.format(tuple(listOfKeys), tuple(listOfValues))
-            # cursor.execute(statement)
-            # conn.commit()
+            print id_[0][u'event_id']
 finally:
     conn.close()
