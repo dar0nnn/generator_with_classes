0e88772bd817ab1c0f9d9f58e83187cad9c498bb
Pavel Lukyanov <panya225@gmail.com>
2018-02-20 14:56:45 +0300

    working version of search in both dbs

--- findMongo.py
+++ findMongo.py
@@ -34,123 +34,124 @@ def timeit(method):
 
     return timed
 
-
-@timeit
-def simpleSmallCount():
-    smallVarFromMongo = events.find({u'severity': u'1', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
-                                     u'created': {u'$gt': datetime.datetime(2017, 12, 31, 23, 00, 00)}})
-    print u'простая маленькая выборка ', smallVarFromMongo.count()
-
-
-@timeit
-def simpleSmallListOfDicts():
-    list = []
-    smallVarFromMongo = events.find({u'severity': u'1', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
-                                     u'created': {u'$gt': datetime.datetime(2017, 12, 31, 23, 00, 00)}})
-    for items in smallVarFromMongo:
-        list.append(items)
-    print u'кол-во словарей в списке простая маленькая выборка: ', len(list)
+class FindForTimeMongo(object):
+    @timeit
+    def simpleSmallCount(self):
+        smallVarFromMongo = events.find({u'severity': u'1', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
+                                         u'created': {u'$gt': datetime.datetime(2017, 12, 31, 23, 00, 00)}})
+        print u'простая маленькая выборка ', smallVarFromMongo.count()
 
 
-@timeit
-def simpleBigCount():
-    bigVarFromMongo = events.find({u'severity': u'0', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
-                                   u'created': {u'$gt': datetime.datetime(2017, 05, 21, 12, 30, 00)}})
-    print u'простая большая выборка ', bigVarFromMongo.count()
+    @timeit
+    def simpleSmallListOfDicts(self):
+        list = []
+        smallVarFromMongo = events.find({u'severity': u'1', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
+                                         u'created': {u'$gt': datetime.datetime(2017, 12, 31, 23, 00, 00)}})
+        for items in smallVarFromMongo:
+            list.append(items)
+        print u'кол-во словарей в списке простая маленькая выборка: ', len(list)
 
 
-@timeit
-def simpleBigListOfDicts():
-    list = []
-    bigVarFromMongo = events.find({u'severity': u'0', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
-                                   u'created': {u'$gt': datetime.datetime(2017, 05, 21, 12, 30, 00)}})
-    for items in bigVarFromMongo:
-        list.append(items)
-    print u'кол-во словарей в списке простая большая выборка: ', len(list)
+    @timeit
+    def simpleBigCount(self):
+        bigVarFromMongo = events.find({u'severity': u'0', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
+                                       u'created': {u'$gt': datetime.datetime(2017, 05, 21, 12, 30, 00)}})
+        print u'простая большая выборка ', bigVarFromMongo.count()
 
 
-@timeit
-def complexSmallCount():
-    smallVarFromMongo = events.find(
-        {u'category': u'3', u'created': {u'$gt': datetime.datetime(2017, 05, 2),
-                                         u'$lte': datetime.datetime(2017, 05, 24)},
-         u'$and': [{u'sourceType': u'2'}, {u'sourceId': u'дополнительный сегмент'}],
-         u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}], u'severity': u'2',
-         u'$and': [{u'params.lType': u'0'},
-                   {u'params.operStatus': u'3'}, {u'params.segment': u'0'}]})
-    print u'сложная маленькая выборка: ', smallVarFromMongo.count()
+    @timeit
+    def simpleBigListOfDicts(self):
+        list = []
+        bigVarFromMongo = events.find({u'severity': u'0', u'params.admStatus': u'0', u'code': u'1.1.1.6.11',
+                                       u'created': {u'$gt': datetime.datetime(2017, 05, 21, 12, 30, 00)}})
+        for items in bigVarFromMongo:
+            list.append(items)
+        print u'кол-во словарей в списке простая большая выборка: ', len(list)
 
 
-@timeit
-def complexSmallListOfDicts():
-    list = []
-    bigVarFromMongo = events.find(
-        {u'category': u'3', u'created': {u'$gt': datetime.datetime(2017, 05, 2),
-                                         u'$lte': datetime.datetime(2017, 05, 24)},
-         u'$and': [{u'sourceType': u'2'}, {u'sourceId': u'дополнительный сегмент'}],
-         u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}], u'severity': u'2',
-         u'$and': [{u'params.lType': u'0'},
-                   {u'params.operStatus': u'3'}, {u'params.segment': u'0'}]})
-    for items in bigVarFromMongo:
-        list.append(items)
-    print u'кол-во словарей в списке сложная маленькая выборка: ', len(list)
+    @timeit
+    def complexSmallCount(self):
+        smallVarFromMongo = events.find(
+            {u'category': u'3', u'created': {u'$gt': datetime.datetime(2017, 05, 2),
+                                             u'$lte': datetime.datetime(2017, 05, 24)},
+             u'$and': [{u'sourceType': u'2'}, {u'sourceId': u'дополнительный сегмент'}],
+             u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}], u'severity': u'2',
+             u'$and': [{u'params.lType': u'0'},
+                       {u'params.operStatus': u'3'}, {u'params.segment': u'0'}]})
+        print u'сложная маленькая выборка: ', smallVarFromMongo.count()
 
 
-@timeit
-def complexBigCount():
-    bigVarFromMongo = events.find(
-        {u'category': u'1', u'created': {u'$gt': datetime.datetime(2017, 05, 1),
-                                         u'$lte': datetime.datetime(2017, 10, 31)},
-         u'$or': [{u'params.lType': u'0', u'params.operStatus': u'3', u'params.segment': u'0'}, {u'params.lType': u'2',
-                                                                                                 u'params.operStatus': u'2',
-                                                                                                 u'params.segment': u'1'}],
-         u'sourceType': u"0",
-         u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}]})
-    print u'сложная большая выборка: ', bigVarFromMongo.count()
+    @timeit
+    def complexSmallListOfDicts(self):
+        list = []
+        bigVarFromMongo = events.find(
+            {u'category': u'3', u'created': {u'$gt': datetime.datetime(2017, 05, 2),
+                                             u'$lte': datetime.datetime(2017, 05, 24)},
+             u'$and': [{u'sourceType': u'2'}, {u'sourceId': u'дополнительный сегмент'}],
+             u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}], u'severity': u'2',
+             u'$and': [{u'params.lType': u'0'},
+                       {u'params.operStatus': u'3'}, {u'params.segment': u'0'}]})
+        for items in bigVarFromMongo:
+            list.append(items)
+        print u'кол-во словарей в списке сложная маленькая выборка: ', len(list)
 
 
-@timeit
-def complexBigListOfDicts():
-    list = []
-    bigVarFromMongo = events.find(
-        {u'category': u'1', u'created': {u'$gt': datetime.datetime(2017, 05, 1),
-                                         u'$lt': datetime.datetime(2017, 10, 31)},
-         u'$or': [{u'params.lType': u'0', u'params.operStatus': u'3', u'params.segment': u'0'}, {u'params.lType': u'2',
-                                                                                                 u'params.operStatus': u'2',
-                                                                                                 u'params.segment': u'1'}],
-         u'sourceType': u"0",
-         u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}]})
-    for items in bigVarFromMongo:
-        list.append(items)
-    print u'кол-во словарей в списке сложная большая выборка: ', len(list)
+    @timeit
+    def complexBigCount(self):
+        bigVarFromMongo = events.find(
+            {u'category': u'1', u'created': {u'$gt': datetime.datetime(2017, 05, 1),
+                                             u'$lte': datetime.datetime(2017, 10, 31)},
+             u'$or': [{u'params.lType': u'0', u'params.operStatus': u'3', u'params.segment': u'0'}, {u'params.lType': u'2',
+                                                                                                     u'params.operStatus': u'2',
+                                                                                                     u'params.segment': u'1'}],
+             u'sourceType': u"0",
+             u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}]})
+        print u'сложная большая выборка: ', bigVarFromMongo.count()
 
 
-@timeit
-def regexSearch():
-    smallVarFromMongo = events.find(
-        {u'$or': [{u'category': u'1'}, {u'category': u'3'}],
-         u'$and': [{u'sourceType': u'0'}, {u'sourceId': u'дополнительный сегмент'}],
-         u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': '1.1.2.6.6'}], u'severity': u'2',
-         u'$and': [{u'params.lType': u'0'},
-                   {u'params.operStatus': u'3'}, {u'params.lName': {u'$regex': u"лол"}}],
-         u'$or': [{u'params.segment': u'0'}, {u'params.segment': u'1'}]})
-    print u'поиск по тексту: ', smallVarFromMongo.count()
+    @timeit
+    def complexBigListOfDicts(self):
+        list = []
+        bigVarFromMongo = events.find(
+            {u'category': u'1', u'created': {u'$gt': datetime.datetime(2017, 05, 1),
+                                             u'$lt': datetime.datetime(2017, 10, 31)},
+             u'$or': [{u'params.lType': u'0', u'params.operStatus': u'3', u'params.segment': u'0'}, {u'params.lType': u'2',
+                                                                                                     u'params.operStatus': u'2',
+                                                                                                     u'params.segment': u'1'}],
+             u'sourceType': u"0",
+             u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}]})
+        for items in bigVarFromMongo:
+            list.append(items)
+        print u'кол-во словарей в списке сложная большая выборка: ', len(list)
 
 
-@timeit
-def totalTime():
-    simpleSmallCount()  # простая маленькая кол-во
-    simpleSmallListOfDicts()  # простая маленькая в переменную
-    simpleBigCount()  # простая большая кол-во
-    simpleBigListOfDicts()  # простая большая в переменную
-    complexSmallCount()  # сложная маленькая кол-во
-    complexSmallListOfDicts()  # сложная маленькая в переменную
-    complexBigCount()  # сложная большая кол-во
-    complexBigListOfDicts()  # сложная большая в переменную
-    regexSearch()  # поиск по фразе в тексте с кучей параметров
-    print u'end of total time func'
-    client.close()
+    @timeit
+    def regexSearch(self):
+        smallVarFromMongo = events.find(
+            {u'$or': [{u'category': u'1'}, {u'category': u'3'}],
+             u'$and': [{u'sourceType': u'0'}, {u'sourceId': u'дополнительный сегмент'}],
+             u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': '1.1.2.6.6'}], u'severity': u'2',
+             u'$and': [{u'params.lType': u'0'},
+                       {u'params.operStatus': u'3'}, {u'params.lName': {u'$regex': u"лол"}}],
+             u'$or': [{u'params.segment': u'0'}, {u'params.segment': u'1'}]})
+        print u'поиск по тексту: ', smallVarFromMongo.count()
+
+
+    @timeit
+    def totalTime(self):
+        self.simpleSmallCount()  # простая маленькая кол-во
+        self.simpleSmallListOfDicts()  # простая маленькая в переменную
+        self.simpleBigCount()  # простая большая кол-во
+        self.simpleBigListOfDicts()  # простая большая в переменную
+        self.complexSmallCount()  # сложная маленькая кол-во
+        self.complexSmallListOfDicts()  # сложная маленькая в переменную
+        self.complexBigCount()  # сложная большая кол-во
+        self.complexBigListOfDicts()  # сложная большая в переменную
+        self.regexSearch()  # поиск по фразе в тексте с кучей параметров
+        print u'end of total time func'
+        client.close()
 
 
 if __name__ == '__main__':
-    totalTime()
+    find = FindForTimeMongo()
+    find.complexSmallCount()
--- test postgres/sql_search.py
+++ test postgres/sql_search.py
@@ -1,43 +1,187 @@
 # -*- coding: UTF-8 -*-
 import csv
 import datetime
+import pprint
+
 import psycopg2.extras
 import psycopg2
 import sys, locale, re
 from writing_to_db import timeit
 from writing_to_db import writingToCsv
+from findMongo import FindForTimeMongo
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 locale.setlocale(locale.LC_ALL, '')
 
-listOfShit = []
-conn = psycopg2.connect(
-    host='192.168.62.129',
-    user='postgres',
-    port=5432,
-    password='qwe123',
-    dbname='Events',
-    client_encoding='utf-8')
-psycopg2.extras.register_hstore(conn, globally=False, unicode=False)
-conn.autocommit = True
-cursor = psycopg2.extras.RealDictCursor(conn)
 
-statement = '''select count(*) from "Events" where ("params" -> 'admStatus' like '%0%') and
-                ("code" like '%1.1.1.6.11%') and ("severity" like '%1%') and
-                ("created" > '2017-05-21 12:30:00')'''
-cursor.execute(statement)
+class FindForFindPostgres(FindForTimeMongo):
+    def __init__(self, cursor):
+        self.cursor = cursor
 
-finded = cursor.fetchall()
-print finded
+    @timeit
+    def simpleSmallCount(self):
+        statement = '''select count(*) from "Events" where
+                       ("params" -> 'admStatus' like '%0%') and
+                       ("code" like '%1.1.1.6.11%') and 
+                       ("severity" like '%1%') and
+                       ("created" > '2017-12-31 23:00:00')'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        print u'простая маленькая выборка ', finded
 
-statement = '''select * from "Events" where ("params" -> 'admStatus' like '%0%') and
-                ("code" like '%1.1.1.6.11%') and ("severity" like '%1%') and
-                ("created" > '2017-05-21 12:30:00')'''
-cursor.execute(statement)
-finded_ = cursor.fetchall()
-# print finded_
-for item in finded_:
-    listOfShit.append(item)
+    @timeit
+    def simpleSmallListOfDicts(self):
+        listOfFinded = []
+        statement = '''select * from "Events" where 
+                      ("params" -> 'admStatus' like '%0%') and
+                      ("code" like '%1.1.1.6.11%') and 
+                      ("severity" like '%1%') and
+                      ("created" > '2017-12-31 23:00:00')'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        for item in finded:
+            listOfFinded.append(item)
+        print u'кол-во словарей в списке простая маленькая выборка: ', len(listOfFinded)
 
-print len(listOfShit)
+    @timeit
+    def simpleBigCount(self):
+        statement = '''select count(*) from "Events" where 
+                       ("params" -> 'admStatus' like '%0%') and
+                       ("code" like '%1.1.1.6.11%') and 
+                       ("severity" like '%1%') and 
+                       ("created" > '2017-05-21 12:30:00')'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        print u'простая большая выборка ', finded
 
+    @timeit
+    def simpleBigListOfDicts(self):
+        listOfFinded = []
+        statement = '''select count(*) from "Events" where
+                       ("params" -> 'admStatus' like '%0%') and
+                       ("code" like '%1.1.1.6.11%') and
+                       ("severity" like '%1%') and
+                       ("created" > '2017-05-21 12:30:00')'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        for item in finded:
+            listOfFinded.append(item)
+        print u'кол-во словарей в списке простая большая выборка: ', len(listOfFinded)
+
+    @timeit
+    def complexSmallCount(self):
+        statement = '''select count(*) from "Events" where 
+         ("category" like '%3%') and
+         ("severity" like '%2%') and 
+         (("created" > '2017-05-2') and 
+          ("created" <= '2017-05-24')) and 
+         (("sourcetype" like '%1%') and 
+          ("sourceid" like '%дополнительный сегмент%')) and 
+         (("params" -> 'lType' like '%0%') and 
+          ("params" -> 'operStatus' like '%3%') and 
+          ("params" -> 'segment' like '%0%')) and 
+         (("code" like '%1.1.2.6.5%') or 
+          ("code" like '%1.1.2.6.6%'));'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        print u'сложная маленькая выборка: ', finded
+
+    @timeit
+    def complexSmallListOfDicts(self):
+        listOfFinded = []
+        statement = '''select * from "Events" where 
+         ("category" like '%3%') and
+         ("severity" like '%2%') and 
+         (("created" > '2017-05-2') and 
+          ("created" <= '2017-05-24')) and 
+         (("sourcetype" like '%1%') and 
+          ("sourceid" like '%дополнительный сегмент%')) and
+         (("params" -> 'lType' like '%0%') and 
+          ("params" -> 'operStatus' like '%3%') and 
+          ("params" -> 'segment' like '%0%')) and
+         (("code" like '%1.1.2.6.5%') or 
+          ("code" like '%1.1.2.6.6%'));'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        for item in finded:
+            listOfFinded.append(item)
+        print u'кол-во словарей в списке сложная маленькая выборка: ', len(listOfFinded)
+
+    # {u'category': u'1', u'created': {u'$gt': datetime.datetime(2017, 05, 1),
+    #                                  u'$lte': datetime.datetime(2017, 10, 31)},
+    #  u'$or': [{u'params.lType': u'0', u'params.operStatus': u'3', u'params.segment': u'0'}, {u'params.lType': u'2',
+    #                                                                                          u'params.operStatus': u'2',
+    #                                                                                          u'params.segment': u'1'}],
+    #  u'sourceType': u"0",
+    #  u'$or': [{u'code': u'1.1.2.6.5'}, {u'code': u'1.1.2.6.6'}]})
+
+
+    @timeit
+    def complexBigCount(self):
+        statement = '''select count(*) from "Events" where 
+         ("category" like '%1%') and
+         ("severity" like '%2%') and 
+         (("created" > '2017-05-1') and ("created" <= '2017-10-31')) and 
+          ("sourcetype" like '%0%') and
+         ((("params" -> 'lType' like '%0%') and 
+           ("params" -> 'operStatus' like '%3%') and 
+           ("params" -> 'segment' like '%0%')) or 
+          (("params" -> 'lType' like '%2%') and
+           ("params" -> 'operStatus' like '%2%') and
+           ("params" -> 'segment' like '%1%'))) and
+         (("code" like '%1.1.2.6.5%') or 
+          ("code" like '%1.1.2.6.6%'));'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        print u'сложная большая выборка: ', finded
+
+    @timeit
+    def complexBigListOfDicts(self):
+        listOfFinded = []
+        statement = '''select * from "Events" where 
+         ("category" like '%1%') and
+         ("severity" like '%2%') and 
+         (("created" > '2017-05-1') and ("created" <= '2017-10-31')) and 
+          ("sourcetype" like '%0%') and
+         ((("params" -> 'lType' like '%0%') and 
+           ("params" -> 'operStatus' like '%3%') and 
+           ("params" -> 'segment' like '%0%')) or 
+          (("params" -> 'lType' like '%2%') and
+           ("params" -> 'operStatus' like '%2%') and
+           ("params" -> 'segment' like '%1%'))) and
+         (("code" like '%1.1.2.6.5%') or 
+          ("code" like '%1.1.2.6.6%'));'''
+        self.cursor.execute(statement)
+        finded = self.cursor.fetchall()
+        for item in finded:
+            listOfFinded.append(item)
+        print u'кол-во словарей в списке сложная большая выборка: ', len(listOfFinded)
+
+    @timeit
+    def totalTime(self):
+        self.simpleSmallCount()
+        self.simpleSmallListOfDicts()
+        self.simpleBigCount()
+        self.simpleBigListOfDicts()
+        self.complexSmallCount()
+        self.complexSmallListOfDicts()
+        self.complexBigCount()
+        self.complexBigListOfDicts()
+        print 'end of total time func'
+
+
+if __name__ == '__main__':
+    conn = psycopg2.connect(
+        host='192.168.62.129',
+        user='postgres',
+        port=5432,
+        password='qwe123',
+        dbname='Events',
+        client_encoding='utf-8')
+    psycopg2.extras.register_hstore(conn, globally=False, unicode=False)
+    conn.autocommit = True
+    cursor = psycopg2.extras.RealDictCursor(conn)
+
+    find = FindForFindPostgres(cursor=cursor)
+    find.totalTime()
